adc97374015f16dc4d620a168a7e422669079611
syedvali98 <syedvali98@gmail.com>
2020-02-14 15:15:08 +0530

    Initial Commit

--- js/main.js
+++ js/main.js
@@ -20,15 +20,7 @@ $(document).ready(function($) {
   $.Scrollax();
 
 
-	// loader
-	var loader = function() {
-		setTimeout(function() { 
-			if($('#ftco-loader').length > 0) {
-				$('#ftco-loader').removeClass('show');
-			}
-		}, 1);
-	};
-	loader();
+	
 
 	var carousel = function() {
 		$('.home-slider').owlCarousel({
